5b75f648c40dec7b9feac1b3026984f3a8b0b53f
Jorge Constable <jorge.constable@gmail.com>
2017-05-10 23:36:53 -0300

    Eliminar cuidador LISTO
    tanto front como back, eliminar listo

--- src/main/java/controlador/CuidadorREST.java
+++ src/main/java/controlador/CuidadorREST.java
@@ -7,7 +7,9 @@ package controlador;
 
 import java.util.List;
 import javax.ejb.Stateless;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -40,6 +42,17 @@ public class CuidadorREST {
     public Cuidador find(@PathParam("id") Long id) throws Exception {
         CuidadorDAO cuidadorDAO = new CuidadorDAO();
         return cuidadorDAO.find(id);
-
+   
+    }
+    
+    @DELETE
+    @Path("{id}")
+    //@Produces(MediaType.APPLICATION_JSON)
+    public void delete(@PathParam("id") Long id) throws Exception {
+        CuidadorDAO cuidadorDAO = new CuidadorDAO();
+        cuidadorDAO.removeID(id);
+        System.out.println("Eliminar "+id);
+        //return cuidadorDAO.find(id);
+   
     }
 }
