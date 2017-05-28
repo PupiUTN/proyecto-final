197b506b35f90fdf93b52326c3298601fe3b2394
Jose Boretto <joseboretto@gmail.com>
2017-05-28 18:17:28 -0300

    Test file rest

--- src/main/java/app/controlador/HelloController.java
+++ src/main/java/app/controlador/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
     
-    @GetMapping("/hello")
+    @GetMapping("api/hello")
     public String index() {
         return "Greetings from Spring Boot!";
     }
