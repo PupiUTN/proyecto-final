21c14947f849a53d1296acdf98fed69ac6e1e755
Fede Backhaus <fbackhaus94@gmail.com>
2017-07-19 00:02:16 -0300

    Changed select for list

--- src/main/webapp/views/vacuna/js/vue_vacuna.js
+++ src/main/webapp/views/vacuna/js/vue_vacuna.js
@@ -5,24 +5,26 @@
 var pupi = new Vue({
     el: '#growler',
     data: {
-        vacunaSeleccionada: ''
+        vacunaSeleccionada: '',
+        listaVacunas: []
+    },
+    created: function() {
+        this.getVacunas();
+    },
+    methods: {
+        getVacunas: function () {
+            const vm = this;
+            axios.get("/api/vacunas")
+                .then(function (response) {
+                    console.log(response);
+                    vm.listaVacunas = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+
     }
 });
 
-var vacunas = function getVacunas() {
-    axios.get("/api/vacunas")
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-        console.log(error);
-    });
-}
 
-Vue.component('vacunas', {
-    template: '<h3>{{ vacunas }}</h3>',
-    // template: '<select v-model="vacunaSeleccionada"><option disabled value="">Seleccionar Vacuna</option><option v-for="vacuna in listaVacunas" :value="vacuna.nombre">{{ vacuna.nombre }} </option> </select>',
-    data: function() {
-        return vacunas
-    }
-});
