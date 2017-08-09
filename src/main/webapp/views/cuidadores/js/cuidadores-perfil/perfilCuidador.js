2da5e487a1481d107aea879e6904c3d070736571
LorenzattiGabriel <gabriellorenzatti94@gmail.com>
2017-08-09 20:41:32 -0300

    cargar perfil cuidador

--- src/main/webapp/views/cuidadores/js/cuidadores-perfil/perfilCuidador.js
+++ src/main/webapp/views/cuidadores/js/cuidadores-perfil/perfilCuidador.js
@@ -11,18 +11,19 @@ let vm = new Vue({
             telefono:'',
             direccion:'',
             cantidadMaxDePerros:'',
-            listaImagenes:''
+            listaImagenes:'',
+            descripcion:'',
+
         },
         items: [],
         cuidador: {},
         formPost: true,
-
+          urlImagen:''
     }
         ,
     mounted() {
         IdCuidador =this.getParameterByName('id');
         this.getItemsAjax(this.url,IdCuidador);
-
 
 
 
@@ -36,6 +37,11 @@ let vm = new Vue({
                 .then((response) => {
                     this.item = response.data[0];
                         this.cuidador = this.item.nombre;
+                    this.item.descripcion= "  Soy un amante de los perros por lo cual puedo cuidar a tu mascota de la mejor manera alir a correr con ellos, jugar y cuidarlos Mis servicios son cuidarlos en el dia y pasearlos, quédate tranquil@ porque tu mascota se divertirá me gustar jugar y estará a salvo. mi zona es Barrio Jardin" +
+                        ", muy cerca de ciudad universitaria y el centro, puedo  en cualquier horario";
+
+                    this.urlImagen= this.item.listaImagenes[0].url;
+                    document.getElementById("myImg").src =  this.urlImagen;
                 })
                 .catch(error => {
                         console.log(error);
