06cda229ef5f235f78e96092a33e862efbc1de8d
Jorge Constable <jorge.constable@gmail.com>
2017-08-15 00:54:37 -0300

    index vue.js y querystring
    index a vue.js, manejo de querystring y arreglado algunas fallas.
    Falta:mostrar en mapa y pasar a perfil

--- src/main/webapp/views/cuidadores/js/cuidadores.js
+++ src/main/webapp/views/cuidadores/js/cuidadores.js
@@ -4,15 +4,12 @@ let vm = new Vue({
         autocomplete:null,
         url: "/api/cuidadores/search",
         encontrados:'',
-        // item: {
-        //     index: '',
-        //     id: '',
-        //     nombre: ''
-        // },
         items: [],
         formPost: true,
-        place:null,
         map:null,
+        placeID:null,
+        placeLocation:null,
+        placeName:null,
 
     },
     mounted() {
@@ -40,21 +37,22 @@ let vm = new Vue({
 
             // Create the autocomplete object, restricting the search to geographical
             // location types
-            autocomplete = new google.maps.places.Autocomplete(input, options);
+            var autocomplete = new google.maps.places.Autocomplete(input, options);
             //PlaceHolder de la busqueda anterior, cambia al cambiar ciudad
             autocomplete.addListener('place_changed', () => {
-                input.placeholder='Ciudad';
-                this.place = autocomplete.getPlace();
-                if (this.place.geometry) {
-                    console.log("econtre");
-                    let placeID = this.place.place_id;
-                    console.log(this.place);
+                input.placeholder = 'Ciudad';
+                let place = autocomplete.getPlace();
+                if (place.geometry) {
+                    this.placeID = place.place_id;
+                    this.placeLocation= place.geometry.location;
+                    this.placeName= input.value;
+
 
 
                 } else {
                     document.getElementById('location').value = '';
-                    // Debe mejorar con un toast
-                    window.alert("No details available for input: '" + place.name + "'");
+
+                    sweetAlert("Oops...", "No se encuentra la ciudad", "error");
                     return;
                 }
 
@@ -65,46 +63,68 @@ let vm = new Vue({
             var argentina = {lat: -37.0230271, lng: -64.6175935};
             this.map = new google.maps.Map(document.getElementById('map'), {
                 zoom: 5,
-                center:argentina,
+                center: argentina,
             });
         },
         getCuidadores() {
-
-            placeID=sessionStorage.getItem('placeID');
-            if(placeID!=null){
+            var input = document.getElementById('location');
+            var placeID = this.getParameterByName('placeID');
+            if (placeID != null) {
                 //el placeholder de ciudad es la ciudad que le pasa el index
-                let busqueda=sessionStorage.getItem('busqueda');
-                var input = document.getElementById('location');
-                input.placeholder=busqueda;
-                this.place=JSON.parse( sessionStorage.getItem('place'));
-                this.map.setCenter(this.place.geometry.location);
+                input.placeholder = this.getParameterByName('placeName');
+                //crea un objeto como los pueda leer google maps
+                var centro = new google.maps.LatLng(this.getParameterByName('lat'), this.getParameterByName('lng'))
+                this.map.setCenter(centro);
                 this.map.setZoom(12);
 
-                axios.get(this.url+'/?ciudadPlaceId='+placeID)
-                .then((response) => {
-                    this.items = response.data;
-                    this.encontrados=this.items.length;
-                    if(this.items.length===1){
-                        this.encontrados+=' Resultado Encontrado';
-                    }else{
-                        this.encontrados+=' Resultados Encontrados';
-                    }
-                    console.log(this.encontrados);
-                    this.toggleLoader();
-                })
-                .catch(error => {
-                        console.log(error);
-                        sweetAlert("Oops...", "Error, ver consola", "error");
-                    }
-                );
-            }else{
+                axios.get(this.url + '/?ciudadPlaceId=' + placeID)
+                    .then((response) => {
+                        this.items = response.data;
+                        this.encontrados = this.items.length;
+                        if (this.items.length === 1) {
+                            this.encontrados += ' Resultado Encontrado';
+                        } else {
+                            this.encontrados += ' Resultados Encontrados';
+                        }
+
+                        this.toggleLoader();
+
+                    })
+                    .catch(error => {
+                            console.log(error);
+                            sweetAlert("Oops...", "Error, ver consola", "error");
+                        }
+                    );
+            } else {
                 this.toggleLoader();
             }
         },
+        //obitene los parametros de la url... copiado de internet
+        getParameterByName(name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
+        },
+        buscar() {
+            if (this.placeID != null) {
+                console.log(this.placeID);
+                window.location.href = "http://localhost:8080/views/cuidadores/lista-cuidadores.html?placeName=" + this.placeName +
+                    "&placeID=" + this.placeID +
+                    "&lat=" + this.placeLocation.lat() +
+                    "&lng=" + this.placeLocation.lng();
+            } else {
+                sweetAlert("Oops...", "Ingrese una ciudad válida", "error");
+                //no se ejecuta en orden... ver mas adelante
+                document.getElementById('location').value = '';
+                document.getElementById('location').placeholder = 'Ciudad';
 
 
-
-
+            }
+        }
     }
 
 
---
+++ src/main/webapp/views/index/js/index.js
@@ -0,0 +1,85 @@
+let vm= new Vue({
+    el:'#appVue',
+    data:{
+        autocomplete:null,
+        placeID:null,
+        placeLocation:null,
+        placeName:null,
+
+    },
+    mounted(){
+        this.initDate();
+        this.initAutocomplete();
+        //this.geolocate();
+
+    },
+    methods:{
+        initDate() {
+            $('#booking-date-from').dateDropper();
+            $('#booking-date-to').dateDropper();
+        },
+        initAutocomplete() {
+            //https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-addressform
+            input = document.getElementById('location');
+            var options = {
+                types: ['(cities)'],
+                componentRestrictions: {country: "ar"}
+            };
+            // Create the autocomplete object, restricting the search to geographical
+            // location types.
+            this.autocomplete = new google.maps.places.Autocomplete(input, options);
+            this.autocomplete.addListener('place_changed', () => {
+                input.placeholder='Ciudad';
+                let place = this.autocomplete.getPlace();
+                if (place.geometry) {
+
+                    this.placeID = place.place_id;
+                    this.placeLocation= place.geometry.location;
+                    this.placeName= input.value;
+
+
+
+                } else {
+                    document.getElementById('location').value = '';
+                    sweetAlert("Oops...", "No se encuentra la ciudad", "error");
+
+                }
+
+        })
+        },
+        geolocate() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    var lat = position.coords.latitude;
+                    var long = position.coords.longitude;
+                    //https://developers.google.com/maps/documentation/geocoding/start
+                    $.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + long + '&sensor=true', function (data) {
+                        var cityStatCountry = data.results[1].formatted_address;
+                        document.getElementById('location').placeholder = cityStatCountry;
+                        //var place = data.results[1];
+                        //autocomplete.set("place", place);
+                    });
+
+
+                });
+            };
+        },
+        buscar() {
+            if (this.placeID != null) {
+                console.log(this.placeID);
+                window.location.href="http://localhost:8080/views/cuidadores/lista-cuidadores.html?placeName="+this.placeName+
+                "&placeID="+this.placeID+
+                "&lat="+this.placeLocation.lat()+
+                "&lng="+this.placeLocation.lng();
+            } else {
+                sweetAlert("Oops...", "Ingrese una ciudad válida", "error");
+                //no se ejecuta en orden... ver mas adelante
+                document.getElementById('location').value='';
+
+            }
+        }
+    },
+});
+
+
+
