add0eef2f39db2fd74249f04cb5d3febc337f3de
Fede Backhaus <fbackhaus94@gmail.com>
2017-09-05 00:27:26 -0300

    Started email configuration, working so far

--- src/main/java/app/controllers/HelloController.java
+++ src/main/java/app/controllers/HelloController.java
@@ -1,15 +1,26 @@
 package app.controllers;
 
+import app.services.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    MailService mailService;
     
     @GetMapping("api/hello")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @PostMapping("api/email")
+    public void sendEmail() {
+        mailService.sendEmail();
     }
     
 }
---
+++ src/main/java/app/services/MailService.java
@@ -0,0 +1,21 @@
+package app.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+    public void sendEmail() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("fbackhaus94@gmail.com");
+        message.setSubject("Prueba Pupi");
+        message.setText("HOLIS");
+        emailSender.send(message);
+    }
+}
