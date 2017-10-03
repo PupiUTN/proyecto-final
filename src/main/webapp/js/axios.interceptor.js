066d19b8412fdfe7580b3cc79b6bbfcb1c91da19
Jose Boretto <joseboretto@gmail.com>
2017-10-03 01:40:55 -0300

    improve page loader

--- src/main/webapp/js/axios.interceptor.js
+++ src/main/webapp/js/axios.interceptor.js
@@ -1,12 +1,13 @@
+var numberOfAjaxCAllPending = 0;
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
+    numberOfAjaxCAllPending++;
     // Do something before request is sent
     $('.pace').show();
     console.log("------------  $('.pace').show();");
     return config;
 }, function (error) {
     // Do something with request error
-    $('.pace').show();
     console.log("------------  error axios interceptors request");
     sweetAlert("Oops...", "Error, ver consola", "error");
     return Promise.reject(error);
@@ -14,18 +15,24 @@ axios.interceptors.request.use(function (config) {
 
 // Add a response interceptor
 axios.interceptors.response.use(function (response) {
+    numberOfAjaxCAllPending--;
+    console.log("------------  Ajax pending", numberOfAjaxCAllPending);
     // Do something with response data
-    $('.pace').hide();
-    console.log("------------  $('.pace').hide();");
+    if (numberOfAjaxCAllPending == 0) {
+        $('.pace').hide();
+        console.log("------------  $('.pace').hide();");
+    }
     return response;
 }, function (error) {
+    console.log("------------  error axios interceptors response");
     // Do something with response error
     $('.pace').hide();
     console.log("------------  $('.pace').hide();");
-    console.log("------------  error axios interceptors response");
     console.log(error.response);
-    if(error.response.config.url !== '/api/user/me'){
+    if (error.response.config.url !== '/api/user/me') {
         sweetAlert("Oops...", "Error, ver consola", "error");
     }
     return Promise.reject(error);
 });
+
+
