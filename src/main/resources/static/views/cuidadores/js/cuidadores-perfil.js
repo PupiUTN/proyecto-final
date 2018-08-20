20cf50f1011ec5806cbd2a8148f02825ddbbecf1
Jose Boretto Blengino <joseboretto@gmail.com>
2018-08-20 14:49:11 -0300

    big fix: borrar del perfil cuidadores los botones face, twiiter

--- src/main/resources/static/views/cuidadores/js/cuidadores-perfil.js
+++ src/main/resources/static/views/cuidadores/js/cuidadores-perfil.js
@@ -235,25 +235,7 @@ let myCuidadorPerfil = Vue.component('my-cuidador-perfil', {
                     <img id="imgTamañoPerro" alt="">
                     <label style=" margin-left: 30px;"> {{item.tamaño}} </label>
                 </div>
-
-
             </div>
-
-
-            <!-- Share / Like -->
-            <div class="listing-share margin-top-40 margin-bottom-40 no-border">
-
-
-                <!-- Share Buttons -->
-                <ul class="share-buttons margin-top-40 margin-bottom-0">
-                    <li><a class="fb-share" href="#"><i class="fa fa-facebook"></i> Share</a></li>
-                    <li><a class="twitter-share" href="#"><i class="fa fa-twitter"></i> Tweet</a></li>
-                    <li><a class="gplus-share" href="#"><i class="fa fa-google-plus"></i> Share</a></li>
-                    <!-- <li><a class="pinterest-share" href="#"><i class="fa fa-pinterest-p"></i> Pin</a></li> -->
-                </ul>
-                <div class="clearfix"></div>
-            </div>
-
         </div>
         <!-- Sidebar / End -->
 
