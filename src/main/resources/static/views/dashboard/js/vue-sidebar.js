add74c30ee310e60a1a60016d42fa1e7175859ca
Jose Boretto Blengino <joseboretto@gmail.com>
2018-10-09 23:26:03 -0300

    colores reserva side bar

--- src/main/resources/static/views/dashboard/js/vue-sidebar.js
+++ src/main/resources/static/views/dashboard/js/vue-sidebar.js
@@ -13,47 +13,45 @@ let mySidebar = Vue.component('my-sidebar', {
                     <li><a href="/views/perros/perros.html"><i class="im im-icon-Dog"></i> Mis Perros</a></li>
                     <li><a><i class="sl sl-icon-layers"></i> Mis Reservas</a>
 					<ul>
-						<li><a href="/views/reserva/mis-reservas-user.html?status=creada-dueño">Solicitadas</a></li>
-						<li><a href="/views/reserva/mis-reservas-user.html?status=aceptada-cuidador">Aceptadas </a></li>
-						<li><a href="/views/reserva/mis-reservas-user.html?status=pagada-dueño">Pagadas </a></li>
-						<li><a href="/views/reserva/mis-reservas-user.html?status=ejecucion">En ejecucion </a></li>
-						<li><a href="/views/reserva/mis-reservas-user.html?status=cerrada">Cerradas </a></li>
-						<li><a href="/views/reserva/mis-reservas-user.html?status=rechazada-dueño">Canceladas </a></li>
+						<li><a href="/views/reserva/mis-reservas-user.html?status=creada-dueño" style="color: #fffde7"><span class="nav-tag" style="background-color: #fffde7"></span> Solicitadas </a></li>
+						<li><a href="/views/reserva/mis-reservas-user.html?status=aceptada-cuidador" style="color: #f0f4c3"><span class="nav-tag" style="background-color: #f0f4c3"></span> Aceptadas </a></li>
+						<li><a href="/views/reserva/mis-reservas-user.html?status=pagada-dueño" style="color: #c8e6c9"><span class="nav-tag" style="background-color: #c8e6c9"></span> Pagadas </a></li>
+						<li><a href="/views/reserva/mis-reservas-user.html?status=ejecucion" style="color: #e3f2fd"><span class="nav-tag" style="background-color: #e3f2fd"></span> En ejecucion </a></li>
+						<li><a href="/views/reserva/mis-reservas-user.html?status=cerrada" style="color: #cfd8dc"> <span class="nav-tag" style="background-color: #cfd8dc"></span> Cerradas </a></li>
+						<li><a href="/views/reserva/mis-reservas-user.html?status=rechazada-dueño" style="color: #ffebee"><span class="nav-tag" style="background-color: #ffebee"></span> Canceladas </a></li>
 					</ul>
 					</li>
-					                    <li><a><i class="sl sl-icon-star"></i> Calificaciones<!--span v-if="pendientesUser > 0" class="nav-tag green">{{pendientesUser}}</span>--></a>
+					    <li><a><i class="sl sl-icon-star"></i> Calificaciones<!--span v-if="pendientesUser > 0" class="nav-tag green">{{pendientesUser}}</span>--></a>
 					                    					                    
-					                  <ul>
-					    <li><a href="/views/reserva/mis-reservas-user.html?status=finalizada">Pendientes </a></li>
-					    <li><a href="/views/reserva/comentarios-dueño.html"> Recibidas </a></li>
-					                       </ul>	
-					                    </li>
-					                    
+					    <ul>
+					        <li><a href="/views/reserva/mis-reservas-user.html?status=finalizada">Pendientes </a></li>
+					        <li><a href="/views/reserva/comentarios-dueño.html"> Recibidas </a></li>
+					     </ul>
+					 </li>
+					                   
 					  <li v-show="role === 'ROLE_CUIDADOR'""><a href="/views/estadisticas/estadistica.html"><i class="sl sl-icon-pie-chart"></i> Mis Estadisticas </a></li>
 					  <li v-show="role === 'ROLE_USER'"><a href="/views/cuidadores/alta-cuidador.html"><i class="sl sl-icon-user"></i> Convertirse</a></li>
 				
 				</ul>  	
                 <ul v-show="role === 'ROLE_CUIDADOR'" data-submenu-title="Cuidador">
                  
-                     <li v-show="role === 'ROLE_CUIDADOR'"><a href="/views/cuidadores/cuidadores-editar.html"><i class="sl sl-icon-book-open"></i> Mi Descripcion</a></li>
-                     <li v-show="role === 'ROLE_CUIDADOR'"><a href="/views/cuidadores/mi-calendario-cuidador.html"><i class="sl sl-icon-calender"></i> Mi Calendario</a></li>
-                     <li v-show="role === 'ROLE_CUIDADOR'"><a><i class="sl sl-icon-layers"></i> Mis Reservas</a> 
+                     <li><a href="/views/cuidadores/cuidadores-editar.html"><i class="sl sl-icon-book-open"></i> Mi Descripcion</a></li>
+                     <li><a href="/views/cuidadores/mi-calendario-cuidador.html"><i class="sl sl-icon-calender"></i> Mi Calendario</a></li>
+                     <li><a><i class="sl sl-icon-layers"></i> Mis Reservas</a>
 					 <ul>
-						<li><a href="/views/reserva/mis-reservas-cuidador.html?status=creada-dueño">Nuevas </a></li>
-						<li><a href="/views/reserva/mis-reservas-cuidador.html?status=aceptada-cuidador">Aceptadas </a></li>
-						<li><a href="/views/reserva/mis-reservas-cuidador.html?status=rechazada-cuidador">Canceladas </a></li>
-						<li><a href="/views/reserva/mis-reservas-cuidador.html?status=pagada-dueño">Pagadas </a></li>
-						<li><a href="/views/reserva/mis-reservas-cuidador.html?status=ejecucion">En ejecucion</a></li>
-			        	<li><a href="/views/reserva/mis-reservas-cuidador.html?status=cerrada">Cerradas </a></li>
-					 </ul>	
-					 </li>
-					                     <li v-show="role === 'ROLE_CUIDADOR'"><a><i class="sl sl-icon-star"></i> Calificaciones<!--<span v-if="pendientesCuidador > 0" class="nav-tag green">{{pendientesCuidador}}</span>--></a>
-					                      <ul>
-					    <li><a href="/views/reserva/mis-reservas-cuidador.html?status=finalizada">Pendientes </a></li>
-					    <li><a href="/views/reserva/comentarios-cuidador.html"> Realizadas </a></li>
-					                       </ul>	
-					                     
-					                     </li>
+						<li><a href="/views/reserva/mis-reservas-cuidador.html?status=creada-dueño" style="color: #fffde7"><span class="nav-tag" style="background-color: #fffde7"></span> Nuevas </a></li>
+						<li><a href="/views/reserva/mis-reservas-cuidador.html?status=aceptada-cuidador" style="color: #f0f4c3"><span class="nav-tag" style="background-color: #f0f4c3"></span> Aceptadas </a></li>
+						<li><a href="/views/reserva/mis-reservas-cuidador.html?status=pagada-dueño" style="color: #c8e6c9"><span class="nav-tag" style="background-color: #c8e6c9"></span> Pagadas </a></li>
+						<li><a href="/views/reserva/mis-reservas-cuidador.html?status=ejecucion" style="color: #e3f2fd"><span class="nav-tag" style="background-color: #e3f2fd"></span> En ejecucion </a></li>
+						<li><a href="/views/reserva/mis-reservas-cuidador.html?status=cerrada" style="color: #cfd8dc"> <span class="nav-tag" style="background-color: #cfd8dc"></span> Cerradas </a></li>
+						<li><a href="/views/reserva/mis-reservas-cuidador.html?status=rechazada-cuidador" style="color: #ffebee"><span class="nav-tag" style="background-color: #ffebee"></span> Canceladas </a></li>
+					 </ul>
+				<li v-show="role === 'ROLE_CUIDADOR'"><a><i class="sl sl-icon-star"></i> Calificaciones</a>
+					<ul>
+					     <li><a href="/views/reserva/mis-reservas-cuidador.html?status=finalizada">Pendientes </a></li>
+					     <li><a href="/views/reserva/comentarios-cuidador.html"> Realizadas </a></li>
+					</ul>
+				</li>
                 </ul>
           
                 <ul v-show="role === 'ROLE_ADMIN'" data-submenu-title="Admin" >
